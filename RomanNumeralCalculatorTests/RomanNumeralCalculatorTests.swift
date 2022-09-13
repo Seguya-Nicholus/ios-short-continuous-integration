d978198aa23750b41f0f92f757dcc8ec222c9100
Seguya-Nicholus <seguyanicholus2015@gmail.com>
2022-09-13 14:48:18 +0300

    add toRoman function unit test

--- RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
+++ RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
@@ -2,34 +2,43 @@
 //  RomanNumeralCalculatorTests.swift
 //  RomanNumeralCalculatorTests
 //
-//  Created by Jarrod Parkes on 11/29/16.
-//  Copyright © 2016 buddybuild. All rights reserved.
+//  Created by Nicholas Sseguya on 13/09/2022.
+//  Copyright © 2022 buddybuild. All rights reserved.
 //
 
 import XCTest
+@testable import RomanNumeralCalculator
 
 class RomanNumeralCalculatorTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
+
+    override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
-    override func tearDown() {
+
+    override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
     }
-    
-    func testExample() {
+
+    func testToRoman() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(toRoman(1), "I")
+        XCTAssertEqual(toRoman(2), "II")
+        XCTAssertEqual(toRoman(3), "III")
+        XCTAssertEqual(toRoman(4), "IV")
+        XCTAssertEqual(toRoman(5), "V")
+        XCTAssertEqual(toRoman(6), "VI")
+        XCTAssertEqual(toRoman(7), "VII")
+        XCTAssertEqual(toRoman(8), "VIII")
+        XCTAssertEqual(toRoman(9), "IX")
+        XCTAssertEqual(toRoman(10), "X")
     }
-    
-    func testPerformanceExample() {
+
+    func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
